ab7ea48c3da7d153dfdf6e454557904a515b00db
Andrew Tsai <a56342003@gmail.com>
2024-11-28 22:27:39 +0800

    Implement NextAuth integration with custom user registration flow
    - Added `route.ts` to handle GET and POST requests for NextAuth using custom auth handlers.
    - Created `Auth.tsx` component with `SignIn` and `SignOut` buttons to manage authentication actions.
    - Modified `Nav.tsx` to include `UserButton` for user authentication state management.
    - Added `UserButton.tsx` component to display user profile or sign-in option based on session.
    - Added UI components (`avatar.tsx`, `dropdown-menu.tsx`) for user interactions and improved visual elements.
    - Implemented `userHelpers.ts` with `getOrCreateUser` function for handling user creation if not already registered.
    - Created `auth.ts` with NextAuth configuration, integrated GitHub provider, and custom `signIn` callback to automatically create users in the database.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,28 @@
+import UserButton from "@/components/UserButton";
 import { CircleHelp, Medal, Menu, Settings } from "lucide-react";
 import { Button } from "./ui/button";
 
-export default function Nav() {
+export default async function Nav() {
   return (
-    <div className="flex w-full justify-between border border-slate-200 p-3">
-      <Button variant="ghost" size="icon">
+    <div className="flex w-full justify-between items-center border border-slate-200">
+      <Button className="m-1" variant="ghost" size="icon">
         <Menu />
       </Button>
-      <div className="mx-2 flex gap-2">
-        <Button variant="ghost" size="icon">
-          <Medal />
-        </Button>
-        <Button variant="ghost" size="icon">
-          <CircleHelp />
-        </Button>
-        <Button variant="ghost" size="icon">
-          <Settings />
-        </Button>
+      <div className="flex items-center">
+        <div className="mx-2 flex gap-2 items-center">
+          <Button variant="ghost" size="icon">
+            <Medal />
+          </Button>
+          <Button variant="ghost" size="icon">
+            <CircleHelp />
+          </Button>
+          <Button variant="ghost" size="icon">
+            <Settings />
+          </Button>
+        </div>
+        <div className="mr-2">
+          <UserButton />
+        </div>
       </div>
     </div>
   );
