a4443dd838f76902bd0c79f7288245411557924e
Andrew Tsai <a56342003@gmail.com>
2025-01-14 20:45:05 +0800

    Improve layout and page styling for better responsiveness
    - Updated root layout to use `h-screen` for full height responsiveness.
    - Added flexbox centering and full width/height styling to the root page component.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({
   return (
     <AuthSessionProvider>
       <ReactQueryProvider>
-        <div className="flex max-w-full flex-col items-center">
+        <div className="flex h-screen max-w-full flex-col items-center">
           <Nav />
           {children}
         </div>
